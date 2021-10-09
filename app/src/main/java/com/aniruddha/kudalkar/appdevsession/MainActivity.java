f2b90b06f122ea5c1fc590c76e864bd1b48576af
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-10-09 13:13:39 +0530

    user interface and design

--- app/src/main/java/com/aniruddha/kudalkar/appdevsession/MainActivity.java
+++ app/src/main/java/com/aniruddha/kudalkar/appdevsession/MainActivity.java
@@ -3,6 +3,9 @@ package com.aniruddha.kudalkar.appdevsession;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btAdd).setOnClickListener(v -> {
+            Log.i("@ani", "Button Clicked");
+
+            EditText etNum1 = findViewById(R.id.etNum1);
+            EditText etNum2 = findViewById(R.id.etNum2);
+            TextView txtRes = findViewById(R.id.txtRes);
+
+            int num1 = Integer.parseInt(etNum1.getText().toString());
+            int num2 =  Integer.parseInt(etNum2.getText().toString());
+
+            txtRes.setText(String.valueOf(num1 + num2));
+
+        }); // java8 lambda
     }
 }
